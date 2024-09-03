7c8ba16d0f6d524c1ee2fbe70282170f31602c46
Kao Saelor <saelor.kao@gmail.com>
2024-09-02 23:18:33 -0700

    work on deposit function

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ def show_balance():
     print(f"Your balance is ${balance:.2f}")
 
 def deposit():
-    pass
+    amount = float(input("Enter an amount to be deposited: "))
+
+    if amount < 0:
+        print("That's not a valid amount")
+    else:
+        return amount
 
 def withdraw():
     pass
@@ -25,7 +30,7 @@ while is_running:
     if choice == '1':
         show_balance()
     elif choice == '2':
-        deposit()
+        balance += deposit()
     elif choice == '3':
         withdraw()
     elif choice == '4':
